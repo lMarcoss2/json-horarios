2dba1b841942b7df7f4da47116256ffd64015cfd
lMarcoss <markos_193@hotmail.com>
2019-05-31 01:44:55 -0500

    documentación del api de carga de archivos horas biblioteca

--- src/main/java/edu/calc/becas/mcarga/hrs/UploadFileAPI.java
+++ src/main/java/edu/calc/becas/mcarga/hrs/UploadFileAPI.java
@@ -3,6 +3,7 @@ package edu.calc.becas.mcarga.hrs;
 import edu.calc.becas.exceptions.GenericException;
 import edu.calc.becas.mcarga.hrs.blibioteca.model.ProcessedFile;
 import edu.calc.becas.mcarga.hrs.read.files.ReadFile;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,7 +24,7 @@ import static edu.calc.becas.utils.ExtensionFile.XLS_EXTENSION;
 /**
  * @author Marcos Santiago Leonardo
  * Universidad de la Sierra Sur (UNSIS)
- * Description:
+ * Description: api genérico para cargar y guardar archivo
  * Date: 5/4/19
  */
 
@@ -35,6 +36,7 @@ public class UploadFileAPI {
     String locationFile;
 
     @PostMapping("/upload")
+    @ApiOperation(value = "Carga de archivo")
     public ProcessedFile uploadFactura(@RequestParam("file") MultipartFile file) throws GenericException {
         String pathfile = saveFile(file);
         ReadFile.readFile(pathfile);
--- src/main/java/edu/calc/becas/mcarga/hrs/blibioteca/api/UploadFileLibraryAPI.java
+++ src/main/java/edu/calc/becas/mcarga/hrs/blibioteca/api/UploadFileLibraryAPI.java
@@ -2,6 +2,7 @@ package edu.calc.becas.mcarga.hrs.blibioteca.api;
 
 import edu.calc.becas.mcarga.hrs.ProcessFile;
 import edu.calc.becas.mcarga.hrs.UploadFileAPI;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author Marcos Santiago Leonardo
  * Universidad de la Sierra Sur (UNSIS)
- * Description:
+ * Description: API para exponer servicios de carga de horas de biblioteca
  * Date: 5/4/19
  */
 @RestController
 @RequestMapping("/hrs-library")
+@Api(description = "Servicios para carga de horas de biblioteca")
 public class UploadFileLibraryAPI extends UploadFileAPI {
 
     @Autowired
