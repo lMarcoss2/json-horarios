80f5e55706399b81a8990edfad5ebeb5f6af67d3
vampisto <luis.perez@meltsan.com>
2019-06-17 10:38:59 -0500

    se agregan Dockerfile

---
+++ execute-docker.sh
@@ -0,0 +1,6 @@
+docker stop calc-beca-services-container
+docker rm calc-beca-services-container
+docker build -t calc-beca-services .
+#docker run --env-file bd.env -d -p 3311:3306 --name calc-beca-container calc-beca
+docker run -dt -p 3311:8080 --name calc-beca-services-container calc-beca-services
+docker start calc-beca-services-container
