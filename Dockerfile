efd7e88c6f88db3d7511b1cd94a2015a2373ad6d
vampisto <luis.perez@meltsan.com>
2019-06-18 17:13:51 -0500

    se agregan la configuracion del dockerfile para la red

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM openjdk:8u181-jre-slim-stretch
-MAINTAINER AMIS
 COPY target/becas-1.jar /opt/becas-1.jar
 #COPY application.properties /application.properties
-EXPOSE 8182
+EXPOSE 8080
 ENTRYPOINT ["java", "-Djava.awt.headless=true", "-Xms512m", "-Xmx512m", "-jar", "/opt/becas-1.jar"]
--- execute-docker.sh
+++ execute-docker.sh
@@ -1,6 +1,6 @@
 docker stop calc-beca-services-container
 docker rm calc-beca-services-container
 docker build -t calc-beca-services .
-#docker run --env-file bd.env -d -p 3311:3306 --name calc-beca-container calc-beca
-docker run -dt -p 8181:8182 --name calc-beca-services-container calc-beca-services
+docker run -dt --net ctrl-beca --ip 192.160.0.14 -p 8181:8080 --name calc-beca-services-container calc-beca-services --link calc-beca-container
 docker start calc-beca-services-container
+#docker run --name ctrl-eval-back-container  --net ctrl-evaluaciones --ip 192.160.0.14 -p 8182:8182 meltsan/ctrl-eval-back:1.0
