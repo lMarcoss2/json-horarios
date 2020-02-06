8563d2acb764533e894e8c9776a3802ba4cdccee
Leo <markos_193@hotmail.com>
2020-02-05 21:43:19 -0600

    cambio servicio de alumnos

--- src/main/java/edu/calc/becas/malumnos/dao/QueriesAlumnos.java
+++ src/main/java/edu/calc/becas/malumnos/dao/QueriesAlumnos.java
@@ -2,12 +2,15 @@ package edu.calc.becas.malumnos.dao;
 
 public class QueriesAlumnos {
 
-    static final String QRY_GET_ALL = "SELECT AL.*, AC.ID_ACTIVIDAD, AC.NOMBRE_ACTIVIDAD\n" +
-            "  FROM ALUMNOS AL,\n" +
-            "      ACTIVIDAD_ALUMNO ACAL,\n" +
-            "      ACTIVIDADES AC\n" +
-            "WHERE 1 = 1 AND AL.ID_ALUMNO = ACAL.ID_ALUMNO\n" +
-            "AND ACAL.ID_ACTIVIDAD = AC.ID_ACTIVIDAD";
+    static final String QRY_GET_ALL = "SELECT AL.*, AA.ID_ACTIVIDAD\n" +
+            "FROM ALUMNOS AL, ALUMNOS_DAT_PERIODO ADP, ACTIVIDAD_ALUMNO AA\n" +
+            "WHERE AL.MATRICULA = ADP.MATRICULA\n" +
+            "  AND ADP.CVE_PERIODO = '1819A'\n" +
+            "  AND ADP.CVE_GRUPO = AA.CVE_GRUPO\n" +
+            "  AND ADP.CVE_LICENCIATURA = AA.CVE_LICENCIATURA\n" +
+            "  AND ADP.CVE_PERIODO = AA.CVE_PERIODO\n" +
+            "  AND AL.ESTATUS = 'S'\n" +
+            "  AND AA.ID_ACTIVIDAD = 2\n";
     static final String QRY_CONDITION_MATRICULA = "\nAND AL.MATRICULA = ?";
     static final String QRY_CONDITION_ESTATUS = " AND AL.ESTATUS = ? ";
     static final String QRY_CONDITION_ACTIVIDAD = "AND ACAL.ID_ACTIVIDAD = ? ";
--- src/main/java/edu/calc/becas/reporte/asistencia/sala/dao/QueriesAsistenciaSala.java
+++ src/main/java/edu/calc/becas/reporte/asistencia/sala/dao/QueriesAsistenciaSala.java
@@ -4,14 +4,20 @@ final class QueriesAsistenciaSala {
     private QueriesAsistenciaSala() {
     }
 
-    static final String GET_ALUMNOS_BY_USER_AND_SCHEDULE = "SELECT HA.ID_HORARIO_ACTIVIDAD,AA.ID_ACTIVIDAD_ALUMNO, A.NOMBRES, A.APE_PATERNO, A.APE_MATERNO\n" +
-            "FROM USUARIOS US, HORARIO_ACTIVIDAD HA, ACTIVIDAD_ALUMNO AA, ALUMNOS_DAT_PERIODO ADP, ALUMNOS A\n" +
+    static final String GET_ALUMNOS_BY_USER_AND_SCHEDULE = "SELECT HA.ID_HORARIO_ACTIVIDAD, AA.ID_ACTIVIDAD_ALUMNO, A.NOMBRES, A.APE_PATERNO, A.APE_MATERNO\n" +
+            "FROM USUARIOS US,\n" +
+            "     HORARIO_ACTIVIDAD HA,\n" +
+            "     ACTIVIDAD_ALUMNO AA,\n" +
+            "     ALUMNOS_DAT_PERIODO ADP,\n" +
+            "     ALUMNOS A\n" +
             "WHERE US.USERNAME = ?\n" +
             "  AND HA.ID_ACTIVIDAD = 2\n" +
             "  AND HA.ID_HORARIO_ACTIVIDAD = ?\n" +
             "  AND US.ID_USUARIO = HA.ID_USUARIO\n" +
-            "    AND HA.ID_HORARIO_ACTIVIDAD = AA.ID_ACTIVIDAD\n" +
-            "    AND AA.ID_ALUMNO = ADP.ID_ALUMNO\n" +
-            "    AND ADP.ID_ALUMNO = A.ID_ALUMNO";
+            "  AND HA.ID_HORARIO_ACTIVIDAD = AA.ID_ACTIVIDAD\n" +
+            "  AND AA.CVE_GRUPO = ADP.CVE_GRUPO\n" +
+            "  AND AA.CVE_LICENCIATURA = ADP.CVE_LICENCIATURA\n" +
+            "  AND AA.CVE_PERIODO = ADP.CVE_PERIODO\n" +
+            "  AND ADP.ID_ALUMNO = A.ID_ALUMNO\n";
 
 }
