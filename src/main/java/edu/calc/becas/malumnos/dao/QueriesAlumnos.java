3e19d050585865d90cd55b414688637772ec725b
vampisto <luis.perez@meltsan.com>
2020-02-14 11:28:08 -0600

    se modifica referencia por matricula en lugar de id

--- src/main/java/edu/calc/becas/malumnos/dao/QueriesAlumnos.java
+++ src/main/java/edu/calc/becas/malumnos/dao/QueriesAlumnos.java
@@ -6,7 +6,7 @@ public class QueriesAlumnos {
             "  FROM ALUMNOS AL,\n" +
             "      ACTIVIDAD_ALUMNO ACAL,\n" +
             "      ACTIVIDADES AC\n" +
-            "WHERE 1 = 1 AND AL.ID_ALUMNO = ACAL.ID_ALUMNO\n" +
+            "WHERE 1 = 1 AND AL.MATRICULA = ACAL.MATRICULA\n" +
             "AND ACAL.ID_ACTIVIDAD = AC.ID_ACTIVIDAD";
     static final String QRY_CONDITION_MATRICULA = "\nAND AL.MATRICULA = ?";
     static final String QRY_CONDITION_ESTATUS = " AND AL.ESTATUS = ? ";
@@ -16,7 +16,7 @@ public class QueriesAlumnos {
     static final String QRY_ADD = "INSERT INTO ALUMNOS (ID_ALUMNO, CURP, MATRICULA, NOMBRES, APE_PATERNO, APE_MATERNO, ESTATUS, FECHA_CREACION, AGREGADO_POR) VALUES\n" +
             "(?,?,?,?,?,?,?,CURDATE(),?)";
 
-    static final String QRY_ADD_ALUMNO_ACTIVIDAD = "INSERT INTO ACTIVIDAD_ALUMNO (ID_ACTIVIDAD, ID_ALUMNO, AGREGADO_POR, FECHA_CREACION) VALUES\n" +
+    static final String QRY_ADD_ALUMNO_ACTIVIDAD = "INSERT INTO ACTIVIDAD_ALUMNO (ID_ACTIVIDAD, MATRICULA, AGREGADO_POR, FECHA_CREACION) VALUES\n" +
             "(?, ?,'ADMIN',CURDATE())";
 
     static final String QRY_ID_ALUMNO = "SELECT max(ID_ALUMNO)+1 FROM ALUMNOS";
